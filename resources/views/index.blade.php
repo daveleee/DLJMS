0482becf89446db01dde70dfb59548fbae705196
Dave Lee <doauy612@gmail.com>
2020-08-09 21:47:07 -0400

    add index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        Index Page
-    </body>
-</html>
+@extends('layout')
+
+@section('title')
+    51Careers Final Round
+@endsection
+
+@section('body')
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="mt-5">51Careers Final Round - Index Page</h1>
+                <a href="/management/manageCandidates">Enter Management System</a>
+                <ul class="list-unstyled">
+                    <li>Bootstrap 4.5.0</li>
+                    <li>jQuery 3.5.1</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Content -->
+@endsection
