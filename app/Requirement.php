b7b2dd7d2f808bc3501d939af7acd2b9c631e4ec
Dave Lee <doauy612@gmail.com>
2020-08-09 21:49:11 -0400

    added requirement model to access the table_requirement

---
+++ app/Requirement.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Requirement extends Model
+{
+    // define table
+    protected $table = 'table_requirements';
+}
