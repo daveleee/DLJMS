d788add1f2ae4f0e437d70ce2758d41a1bd44f55
Dave Lee <doauy612@gmail.com>
2020-08-10 04:35:35 -0400

    added PHP, MySQL versions

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,10 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1 class="mt-5">51Careers Final Round - Index Page</h1>
-                <a href="/management/manageCandidates">Enter Management System</a>
+                <h1 class="mt-5">51Careers Final Round</h1>
                 <ul class="list-unstyled">
+                    <li>PHP Laravel 7.3.11</li>
+                    <li>MySQL Workbench 8.0.19</li>
                     <li>Bootstrap 4.5.0</li>
                     <li>jQuery 3.5.1</li>
                 </ul>
