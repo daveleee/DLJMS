685f20edc69f04aecf15adbb309ec568b4f24ac3
Dave Lee <doauy612@gmail.com>
2020-08-09 21:44:58 -0400

    add job post model to utilize the requirement table

---
+++ app/JobPost.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class JobPost extends Model
+{
+    // define table
+    protected $table = 'table_jobposts';
+}
